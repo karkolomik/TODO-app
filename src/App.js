7e5c094bd5a6d292fb23a73c0ee933ea710f7afd
Karen Khachatrian <karkolom@gmail.com>
2022-12-05 20:39:50 +0200

    second

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 import TodoForm from './components/Todos/TodoForm';
 import TodoList from './components/Todos/TodoList';
@@ -7,10 +8,19 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   const addTodoHandler = (text) => {
-    setTodos([...todos, text]);
+    const newTodo = {
+      text: text,
+      isComplited: false,
+      id: uuidv4(),
+    };
+    setTodos([...todos, newTodo]);
   };
-  const deleteTodoHandler = (index) => {
-    setTodos(todos.filter((_, idx) => idx !== index));
+  const deleteTodoHandler = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleTodoHandler = (id) => {
+    setTodos(todos.map(todo));
   };
   return (
     <div className="App">
--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,10 +1,16 @@
-import { RiTodoFill } from 'react-icons/ri';
+import { RiDeleteBin2Line, RiTodoFill } from 'react-icons/ri';
+import { FaCheck } from 'react-icons/fa';
 import styles from './Todo.module.css';
-function Todo({ todo, index, deleteTodo }) {
+function Todo({ todo, deleteTodo }) {
   return (
-    <div className={styles.todo} onDoubleClick={() => deleteTodo(index)}>
+    <div className={styles.todo}>
       <RiTodoFill className={styles.todoIcon} />
-      <div className={styles.todoText}>{todo}</div>
+      <div className={styles.todoText}>{todo.text}</div>
+      <RiDeleteBin2Line
+        onClick={() => deleteTodo(todo.id)}
+        className={styles.deleteIcon}
+      />
+      <FaCheck className={styles.checkIcon} />
     </div>
   );
 }
